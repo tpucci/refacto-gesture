6db08d787f08b4f5ccff8cf840687624dea92250
Thomas Pucci <thomas.lb.pucci@gmail.com>
2019-05-29 14:55:33 +0200

    :recycle: (main) Extract variables

--- main.js
+++ main.js
@@ -3,11 +3,11 @@ function main(quantity, itemPrice) {
     quantity,
     itemPrice
   };
-  return (
-    order.quantity * order.itemPrice -
-    Math.max(0, order.quantity - 500) * order.itemPrice * 0.05 +
-    Math.min(order.quantity * order.itemPrice * 0.1, 100)
-  );
+  const basePrice = order.quantity * order.itemPrice;
+  const quantityDiscount =
+    Math.max(0, order.quantity - 500) * order.itemPrice * 0.05;
+  const shippingPrice = Math.min(order.quantity * order.itemPrice * 0.1, 100);
+  return basePrice - quantityDiscount + shippingPrice;
 }
 
 console.log(main(5, 8));
